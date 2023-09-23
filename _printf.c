e6c2cdaec509a1d335333bcb364db5234f85ce72
Komelaws <solomonlaurens@gmail.com>
2023-09-22 21:52:04 -0700

    Updates

--- _printf.c
+++ _printf.c
@@ -8,4 +8,31 @@
  */
 int _printf(const char *format, ...)
 {
+	int num, j, i;
+	char *s;
+	va_list list;
+
+	va_start(list, format);
+	num = strlen(format);
+	i = 0;
+	j = 0;
+
+	while (i < num)
+	{
+		if (format[j] == '%')
+		{
+			j++;
+			if (format[j] == 's')
+			{
+				printf("%s", va_arg(list, const char *));
+			}
+			else if (format[j] == 'c')
+			{
+				printf("%c", va_arg(list, const char *));
+			}
+		}
+		i++;
+	}
+
+	return (0);
 }
